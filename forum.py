ebbef2253203438d3b701b08b21f75c67562f578
InfaNaSotku <InfaNaSotku@gmail.com>
2023-02-07 18:50:40 +0300

    Add templates

--- forum.py
+++ forum.py
@@ -1,10 +1,21 @@
 from flask import Flask
-
-
+from flask import request
+from flask import render_template
 
 forum = Flask(__name__)
 
 
 @forum.route("/")
-def hello():
+def index():
     return "<p>Hi!</p>"
+
+
+
+@forum.route("/login", methods=['POST','GET'])
+def login():
+    return render_template("login.html")
+
+
+
+
+
