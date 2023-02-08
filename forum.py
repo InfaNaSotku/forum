229bb8a204259e7cfd966c2daba13f85181d1649
InfaNaSotku <InfaNaSotku@gmail.com>
2023-02-08 07:46:34 +0300

    Add login form

--- forum.py
+++ forum.py
@@ -1,20 +1,35 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import session
+from flask import redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 forum = Flask(__name__)
-
+db = SQLAlchemy()
+forum.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
+db.init_app(forum)
+forum.secret_key = "SecretKey"
 
 @forum.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", username=session['username'])
 
 
 
 @forum.route("/login", methods=['POST','GET'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        if 'username' in session:
+            return redirect(url_for('index'))
+        else:
+            return render_template("login.html")
 
+ 
 
 
 
