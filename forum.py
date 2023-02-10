d6f10280ea213fb8d26dd7f9c5e1891e01166024
InfaNaSotku <InfaNaSotku@gmail.com>
2023-02-10 16:48:59 +0300

    Upgrade login form

--- forum.py
+++ forum.py
@@ -14,6 +14,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=True, nullable=False)
+with forum.app_context():
+    db.create_all()
+
+def add_user(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        user = User(
+            username=username,
+            password=password,
+        )
+        db.session.add(user)
+        db.session.commit()
+    return user
 
 
 @forum.route('/')
@@ -23,11 +36,12 @@ def index():
     else:
         return render_template('index.html', username='guest')
 
-
-
 @forum.route('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
+        user = add_user(request.form['username'], request.form['password'])
+        if not user is None and not user.password == request.form['password']:
+            return render_template('login.html', error='error')
         session['username'] = request.form['username']
         session['password'] = request.form['password']
         return redirect(url_for('index'))
