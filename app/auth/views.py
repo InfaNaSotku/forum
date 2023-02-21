b37b6dfffc9b517552f8de916c5a9c14ad6e678e
InfaNaSotku <InfaNaSotku@gmail.com>
2023-02-21 21:15:36 +0700

    Upgrade authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, render_template, request, session, url_for
-from app.model_manage import add_user
+from app.model_manage import add_user, get_user, delete_user
 from . import auth
+from app.model import User
 
 @auth.route('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
-        user = add_user(request.form['username'], request.form['password'])
-        if not user is None and not user.password == request.form['password']:
-            return render_template('login.html', error='error')
+        print("smth")
+        user = get_user(request.form['username'])
+        if not user:
+            return render_template('login.html', error='User doesn\'t exist')
+        if not user.password == request.form['password']:
+            return render_template('login.html', error='Incorrect password')
         session['username'] = request.form['username']
         session['password'] = request.form['password']
         return redirect(url_for('main.index'))
@@ -17,8 +21,32 @@ def login():
         else:
             return render_template('login.html')
 
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        if not request.form['password'] == request.form['confirm password']:
+            return render_template('register.html', error='Password doesn\'t match')
+        if get_user(request.form['username']):
+            return render_template('register.html', error='User already exist')
+        add_user(request.form['username'], request.form['password'])
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        if 'username' in session:
+            return redirect(url_for('main.index'))
+        else:
+            return render_template('register.html')
+
 @auth.route('/logout')
 def logout():
     if 'username' in session:
+        session.clear()
+    return redirect(url_for('main.index'))
+
+@auth.route('/delete')
+def delete():
+    if 'username' in session:
+        delete_user(username=session['username'])
         session.clear()
     return redirect(url_for('main.index'))
--- app/model_manage.py
+++ app/model_manage.py
@@ -1,13 +1,31 @@
 from .model import User, Question, Comment
 from app import db
+from datetime import datetime
+from flask import session
 
 def add_user(username : str, password : str):
-    user = User.query.filter_by(username=username).first()
-    if user is None:
-        user = User(
-            username=username,
-            password=password,
-        )
-        db.session.add(user)
-        db.session.commit()
+    user = User(
+        username=username,
+        password=password,
+    )
+    db.session.add(user)
+    db.session.commit()
     return user
+
+def delete_user(username : str):
+    db.session.delete(User.query.filter_by(username=username).first())
+    db.session.commit()
+
+def get_user(username : str) -> bool:
+    return User.query.filter_by(username=username).first()
+
+def add_question(title : str, content=""):
+    question = Question(
+        title = title,
+        content = content,
+        date = datetime.now(),
+        user = User.query.filter_by(username=session['username']).first()
+    )
+    db.session.add(question)
+    db.session.commit()
+    return question
